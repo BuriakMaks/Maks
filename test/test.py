fa7f263f3b4ae05f8c18e9f98a192fce461049fa
BuriakMaks <fenix.kiev.maks@gmail.com>
2023-07-27 20:26:38 +0300

    Initial commit

--- test/test.py
+++ test/test.py
@@ -1,8 +1,26 @@
-age = 12
-name = 'Vasia'
-print(type(age))
+ab = 10
+bc = 20
+cd = 30
+de = 40
+ef = 10
 
+a_f = (10, 20, 30, 40, 10)
 
+un = set(a_f)
+sun = sum(un)
 
-dct = {'name': 'vasia', 'age': 15}
+shahta = {
+    "Klikyha" : "ab",
+    "Habar" : 10
+}
+shahta["Habar"] = shahta["Habar"] * 1.5
 
+#1
+#1.1 print(ab+bc+cd+de+ef)
+print(sum(a_f))
+
+#2
+print(sun)
+
+#3
+print(shahta)
